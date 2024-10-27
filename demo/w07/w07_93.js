9f4a2e23b1cd86be7bda52966f1dc7e6a1d2a40c
JonasReinhard0427 <jonas427919@gmail.com>
2024-10-28 00:18:10 +0800

    redo w07 folder rewrite js code of iframe

--- demo/w07/w07_93.js
+++ demo/w07/w07_93.js
@@ -4,13 +4,13 @@ console.log('show-classdemo', p);
 function showDemo(item) {
   switch (item) {
     case 'w02_intro_93':
-      p.innerHTML = `<iframe src='./w02_intro_93/index.html' width="100%" height="100%"></iframe>`;
+      p.innerHTML = `<iframe src='./demo/w07/w02_intro_93/index.html' width="100%" height="100%"></iframe>`;
       break;
     case 'w03_card_93':
-      p.innerHTML = `<iframe src='./w03_card_93/index.html' width="100%" height="100%"></iframe>`;
+      p.innerHTML = `<iframe src='./demo/w07/w03_card_93/index.html' width="100%" height="100%"></iframe>`;
       break;
     case 'w06_blog_93':
-      p.innerHTML = `<iframe src='./w06_blog_93/index.html' width="100%" height="100%"></iframe>`;
+      p.innerHTML = `<iframe src='./demo/w07/w06_blog_93/index.html' width="100%" height="100%"></iframe>`;
       break;
   }
 }
