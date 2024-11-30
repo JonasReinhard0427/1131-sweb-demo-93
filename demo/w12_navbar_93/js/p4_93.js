f6594ab7e5dd863842f1f03aff0dc17d0bac6076
cdchiang <jonas427919@gmail.com>
2024-11-30 15:23:32 +0800

    add w12 p1-2 & 1-3

--- demo/w12_navbar_93/js/p4_93.js
+++ demo/w12_navbar_93/js/p4_93.js
@@ -7,4 +7,15 @@ function showMidtermPage(item) {
             p.innerHTML = `<iframe src='./p1_93/p1_93.html' width='100%' height='100%' />`
             break;
     }
+    switch (item) {
+        case 'p2_93':
+            p.innerHTML = `<iframe src='./p2_93/p2_93.html' width='100%' height='100%' />`
+            break;
+    }
+    switch (item) {
+        case 'p3_93':
+            p.innerHTML = `<iframe src='./p3_93/p3_93.html' width='100%' height='100%' />`
+            break;
+    }
+
 }
